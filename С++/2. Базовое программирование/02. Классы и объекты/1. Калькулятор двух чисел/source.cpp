c0614f8cebf8855dd69bd59858437b02c0ea41ca
Konstantin Donchenko <196397907+KDprogs@users.noreply.github.com>
2025-02-19 15:59:11 +0200

    Добавлено "Базовое программирование на C++" Классы и объекты

---
+++ С++/2. Базовое программирование/02. Классы и объекты/1. Калькулятор двух чисел/source.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+class Calculator
+{
+public:
+	bool set_num1(double num1)
+	{
+		if (num1 != 0)
+		{
+			this->num1 = num1;
+			return true;
+		}
+		return false;
+	}
+
+	bool set_num2(double num2)
+	{
+		if (num2 != 0)
+		{
+			this->num2 = num2;
+			return true;
+		}
+		return false;
+	}
+
+	double add() const { return num1 + num2; }
+	double multiply() const { return num1 * num2; }
+	double subtract_1_2() const { return num1 - num2; }
+	double subtract_2_1() const { return num2 - num1; }
+	double divide_1_2() const { return num1 / num2; }
+	double divide_2_1() const { return num2 / num1; }
+
+private:
+	double num1;
+	double num2;
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	double num1{};
+	double num2{};
+	bool set{};
+	Calculator calc;
+
+	do
+	{
+		do
+		{
+			std::cout << "Введите num1: ";
+			std::cin >> num1;
+			set = calc.set_num1(num1);
+			if (!set) { std::cout << "Неверный ввод!\n"; }
+
+		} while (!set);
+
+		do
+		{
+			std::cout << "Введите num2: ";
+			std::cin >> num2;
+			set = calc.set_num2(num2);
+			if (!set) { std::cout << "Неверный ввод!\n"; }
+
+		} while (!set);
+
+		std::cout << "num1 + num2 = " << calc.add() << '\n'
+			<< "num1 - num2 = " << calc.subtract_1_2() << '\n'
+			<< "num2 - num1 = " << calc.subtract_2_1() << '\n'
+			<< "num1 * num2 = " << calc.multiply() << '\n'
+			<< "num1 / num2 = " << calc.divide_1_2() << '\n'
+			<< "num2 / num1 = " << calc.divide_2_1() << '\n';
+
+	} while (set);
+
+	return 0;
+}
---
+++ С++/2. Базовое программирование/02. Классы и объекты/2. Счётчик/source.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <windows.h>
+
+class Counter
+{
+public:
+	Counter() { count = 1; }
+	Counter(int num) { count = num; }
+
+	void setIncrease() { count++; }
+	void setDecrease() { count--; }
+	void getCurrent() const { std::cout << count << '\n'; }
+
+private:
+	int count;
+};
+
+void commands(Counter& score);
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string answer{};
+	int num{};
+	
+	std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+	std::cin >> answer;
+	if (answer == "да")
+	{
+		std::cout << "Введите начальное значение счётчика: ";
+		std::cin >> num;
+		Counter score(num);
+		commands(score);
+	}
+	else
+	{
+		Counter score;
+		commands(score);
+	}
+
+	std::cout << "До свидания!\n";
+
+	return 0;
+}
+
+
+void commands(Counter& score)
+{
+	char command{};
+
+	do
+	{
+		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		std::cin >> command;
+
+		switch (command)
+		{
+			case '+': score.setIncrease(); break;
+			case '-': score.setDecrease(); break;
+			case '=': score.getCurrent();
+		}
+
+	} while (command != 'x');
+}
