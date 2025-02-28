8488d71eb15b167f62662570da95e4fb78ff9403
Konstantin Donchenko <196397907+KDprogs@users.noreply.github.com>
2025-02-28 15:23:19 +0200

    Добавлено "Базовое программирование на C++" ООП: наследование и полиморфизм

---
+++ С++/2. Базовое программирование/04. ООП наследование и полиморфизм/1. Фигуры. Количество сторон/source.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+class Figure
+{
+public:
+	Figure() = default;
+
+	unsigned get_sides_count() const { return sides_count; }
+	std::string get_name() const { return name; }
+
+protected:
+	Figure(unsigned sides_count) : sides_count(sides_count) {}
+	std::string name{ "Фигура" };
+
+private:
+	unsigned sides_count{};
+};
+
+
+class Triangle : public Figure
+{
+public:
+	Triangle() : Figure(3)
+	{
+		name = "Треугольник";
+	}
+};
+
+
+class Quadrangle : public Figure
+{
+public:
+	Quadrangle() : Figure(4)
+	{
+		name = "Четырёхугольник";
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	Figure fi;
+	Triangle tr;
+	Quadrangle qu;
+
+	std::cout << "Количество сторон:\n"
+		<< fi.get_name() << ": " << fi.get_sides_count() << '\n'
+		<< tr.get_name() << ": " << tr.get_sides_count() << '\n'
+		<< qu.get_name() << ": " << qu.get_sides_count() << '\n';
+
+	return 0;
+}
---
+++ С++/2. Базовое программирование/04. ООП наследование и полиморфизм/3. Фигуры. Методы/source.cpp
@@ -0,0 +1,293 @@
+﻿#include <iostream>
+#include <string>
+
+class Figure
+{
+public:
+	Figure() = default;
+
+	virtual std::string print_info()
+	{
+		return get_name() + ":\n" + (check()?"Правильная":"Неправильная") + "\nКоличество сторон: " + std::to_string(get_sides_count()) + '\n';
+	}
+	virtual bool check()
+	{
+		if (sides_count == 0) { return true; }
+		else { return false; }
+	};
+	unsigned get_sides_count() const { return sides_count; }
+	std::string get_name() const { return name; }
+
+protected:
+	Figure(unsigned sides_count) : sides_count(sides_count) {}
+	std::string name{ "Фигура" };
+
+private:
+	unsigned sides_count{};
+};
+
+
+class Triangle : public Figure
+{
+public:
+	Triangle() : Figure(3), a(10), b(20), c(30), A(50), B(60), C(70)
+	{
+		name = "Треугольник";
+	}
+
+	std::string print_info() override
+	{
+		return Figure::print_info() +
+			"Стороны: a=" + std::to_string(a) + " b=" + std::to_string(b) + " c=" + std::to_string(c) +
+			"\nУглы: A=" + std::to_string(A) + " B=" + std::to_string(B) + " C=" + std::to_string(C) + '\n';
+	}
+	bool check() override
+	{
+		if (get_sides_count() == 3 && A + B + C == 180) { return true; }
+		else { return false; }
+	};
+	unsigned get_a() const { return a; }
+	unsigned get_b() const { return b; }
+	unsigned get_c() const { return c; }
+	unsigned get_A() const { return A; }
+	unsigned get_B() const { return B; }
+	unsigned get_C() const { return C; }
+
+protected:
+	Triangle(unsigned a, unsigned b, unsigned c, unsigned A, unsigned B, unsigned C)
+		: Figure(3), a(a), b(b), c(c), A(A), B(B), C(C) {
+	}
+	Triangle(unsigned a, unsigned b, unsigned c, unsigned A, unsigned B)
+		: Figure(3), a(a), b(b), c(c), A(A), B(B) {
+		C = 90;
+	}
+	Triangle(unsigned a, unsigned b, unsigned A, unsigned B)
+		: Figure(3), a(a), b(b), A(A), B(B) {
+		c = a; C = A;
+	}
+	Triangle(unsigned a)
+		: Figure(3), a(a) {
+		b = a; c = a; A = 60; B = 60; C = 60;
+	}
+
+private:
+	unsigned a{};
+	unsigned b{};
+	unsigned c{};
+	unsigned A{};
+	unsigned B{};
+	unsigned C{};
+};
+
+
+class Right_triangle : public Triangle
+{
+public:
+	Right_triangle() : Triangle(10, 20, 30, 50, 40)
+	{
+		name = "Прямоугольный треугольник";
+	}
+
+	bool check() override
+	{
+		if (Triangle::check() && get_C() == 90) { return true; }
+		else { return false; }
+	};
+};
+
+
+class Isosceles_triangle : public Triangle
+{
+public:
+	Isosceles_triangle() : Triangle(10, 20, 50, 60)
+	{
+		name = "Равнобедренный треугольник";
+	}
+
+	bool check() override
+	{
+		if (Triangle::check() && get_a() == get_c() && get_A() == get_C()) { return true; }
+		else { return false; }
+	};
+};
+
+
+class Equilateral_triangle : public Triangle
+{
+public:
+	Equilateral_triangle() : Triangle(30)
+	{
+		name = "Равносторонний треугольник";
+	}
+
+	bool check() override
+	{
+		if (Triangle::check() && get_a() == get_b() && get_b() == get_c() && get_A() == get_B() && get_B() == get_C()) { return true; }
+		else { return false; }
+	};
+};
+
+
+class Quadrangle : public Figure
+{
+public:
+	Quadrangle() : Figure(4), a(10), b(20), c(30), d(40), A(50), B(60), C(70), D(80)
+	{
+		name = "Четырёхугольник";
+	}
+
+	std::string print_info() override
+	{
+		return Figure::print_info() +
+			"Стороны: a=" + std::to_string(a) + " b=" + std::to_string(b) + " c=" + std::to_string(c) + " d=" + std::to_string(d) +
+			"\nУглы: A=" + std::to_string(A) + " B=" + std::to_string(B) + " C=" + std::to_string(C) + " D=" + std::to_string(D) + '\n';
+	}
+	bool check() override
+	{
+		if (get_sides_count() == 4 && A + B + C + D == 360) { return true; }
+		else { return false; }
+	};
+	unsigned get_a() const { return a; }
+	unsigned get_b() const { return b; }
+	unsigned get_c() const { return c; }
+	unsigned get_d() const { return d; }
+	unsigned get_A() const { return A; }
+	unsigned get_B() const { return B; }
+	unsigned get_C() const { return C; }
+	unsigned get_D() const { return D; }
+
+protected:
+	Quadrangle(unsigned a, unsigned b, unsigned c, unsigned d, unsigned A, unsigned B, unsigned C, unsigned D)
+		: Figure(4), a(a), b(b), c(c), d(d), A(A), B(B), C(C), D(D) {
+	}
+	Quadrangle(unsigned a, unsigned b)
+		: Figure(4), a(a), b(b) {
+		c = a; d = b; A = 90; B = 90; C = 90; D = 90;
+	}
+	Quadrangle(unsigned a)
+		: Figure(4), a(a) {
+		b = a; c = a; d = a; A = 90; B = 90; C = 90; D = 90;
+	}
+	Quadrangle(unsigned a, unsigned b, unsigned A, unsigned B)
+		: Figure(4), a(a), b(b), A(A), B(B) {
+		c = a; d = b; C = A; D = B;
+	}
+	Quadrangle(unsigned a, unsigned A, unsigned B)
+		: Figure(4), a(a), A(A), B(B) {
+		b = a; c = a; d = a; C = A; D = B;
+	}
+
+private:
+	unsigned a{};
+	unsigned b{};
+	unsigned c{};
+	unsigned d{};
+	unsigned A{};
+	unsigned B{};
+	unsigned C{};
+	unsigned D{};
+};
+
+
+class Rectangle : public Quadrangle
+{
+public:
+	Rectangle() : Quadrangle(10, 20)
+	{
+		name = "Прямоугольник";
+	}
+
+	bool check() override
+	{
+		if (Quadrangle::check() && get_a() == get_c() && get_b() == get_d() && get_A() == get_B() && get_B() == get_C() && get_C() == get_D()) { return true; }
+		else { return false; }
+	};
+};
+
+
+class Square : public Quadrangle
+{
+public:
+	Square() : Quadrangle(20)
+	{
+		name = "Квадрат";
+	}
+
+	bool check() override
+	{
+		if (Quadrangle::check() && get_a() == get_b() && get_b() == get_c() && get_c() == get_d() && get_A() == get_B() && get_B() == get_C() && get_C() == get_D()) { return true; }
+		else { return false; }
+	};
+};
+
+
+class Parallelogram : public Quadrangle
+{
+public:
+	Parallelogram() : Quadrangle(20, 30, 30, 40)
+	{
+		name = "Параллелограмм";
+	}
+
+	bool check() override
+	{
+		if (Quadrangle::check() && get_a() == get_c() && get_b() == get_d() && get_A() == get_C() && get_B() == get_D()) { return true; }
+		else { return false; }
+	};
+};
+
+
+class Rhombus : public Quadrangle
+{
+public:
+	Rhombus() : Quadrangle(30, 30, 40)
+	{
+		name = "Ромб";
+	}
+
+	bool check() override
+	{
+		if (Quadrangle::check() && get_a() == get_b() && get_b() == get_c() && get_c() == get_d() && get_A() == get_C() && get_B() == get_D()) { return true; }
+		else { return false; }
+	};
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	Figure fi;
+	Triangle tr;
+	Right_triangle rt;
+	Isosceles_triangle it;
+	Equilateral_triangle et;
+	Quadrangle qu;
+	Rectangle re;
+	Square sq;
+	Parallelogram pa;
+	Rhombus rh;
+
+	Figure* fi1 = &tr;
+	Figure* fi2 = &rt;
+	Figure* fi3 = &it;
+	Figure* fi4 = &et;
+	Figure* fi5 = &qu;
+	Figure* fi6 = &re;
+	Figure* fi7 = &sq;
+	Figure* fi8 = &pa;
+	Figure* fi9 = &rh;
+
+	std::cout << fi.print_info() << '\n'
+		<< fi1->print_info() << '\n'
+		<< fi2->print_info() << '\n'
+		<< fi3->print_info() << '\n'
+		<< fi4->print_info() << '\n'
+		<< fi5->print_info() << '\n'
+		<< fi6->print_info() << '\n'
+		<< fi7->print_info() << '\n'
+		<< fi8->print_info() << '\n'
+		<< fi9->print_info() << '\n';
+
+	return 0;
+}
